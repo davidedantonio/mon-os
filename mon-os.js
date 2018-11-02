2b0f99fea5ac848b9d2b7c404eee9d0a7e5eb3d3
davidedantonio <davide.dantonio@webeetle.com>
2018-11-02 08:28:16 +0100

    fix

--- mon-os.js
+++ mon-os.js
@@ -1,12 +1,3 @@
 'use strict'
 
 const NodeOsInfo = require('./lib/node-os-info')
-const os = new NodeOsInfo()
-
-os.getProcesses(20)
-  .then((result) => {
-    console.log(result)
-  })
-  .catch(e => {
-    console.log(e)
-  })
